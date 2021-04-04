b5238b255b6e289d0179f72fc885a32529dfa29d
warukira <dwarukira@gmail.com>
2021-04-04 16:59:33 +0300

    Add url to get stuff

--- f.sh
+++ f.sh
@@ -1,10 +1,10 @@
 
 
-python manage.py fuckit --filename=/Users/warukira/Downloads/final.xlsx --year=2013
+python manage.py fuckit --filename=https://res.cloudinary.com/dhgp3cxes/raw/upload/v1617544703/final.xlsx --year=2013
 
-python manage.py fuckit --filename=/Users/warukira/Downloads/final.xlsx --year=2014
-python manage.py fuckit --filename=/Users/warukira/Downloads/final.xlsx --year=2015
-python manage.py fuckit --filename=/Users/warukira/Downloads/final.xlsx --year=2016
-python manage.py fuckit --filename=/Users/warukira/Downloads/final.xlsx --year=2017
-python manage.py fuckit --filename=/Users/warukira/Downloads/final.xlsx --year=2018
-python manage.py fuckit --filename=/Users/warukira/Downloads/final.xlsx --year=2019
+python manage.py fuckit --filename=https://res.cloudinary.com/dhgp3cxes/raw/upload/v1617544703/final.xlsx --year=2014
+python manage.py fuckit --filename=https://res.cloudinary.com/dhgp3cxes/raw/upload/v1617544703/final.xlsx --year=2015
+python manage.py fuckit --filename=https://res.cloudinary.com/dhgp3cxes/raw/upload/v1617544703/final.xlsx --year=2016
+python manage.py fuckit --filename=https://res.cloudinary.com/dhgp3cxes/raw/upload/v1617544703/final.xlsx --year=2017
+python manage.py fuckit --filename=https://res.cloudinary.com/dhgp3cxes/raw/upload/v1617544703/final.xlsx --year=2018
+python manage.py fuckit --filename=https://res.cloudinary.com/dhgp3cxes/raw/upload/v1617544703/final.xlsx --year=2019
